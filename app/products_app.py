859774d93aea6db4ce16944507ad5dad9257a439
Jeffrey Kern <jwkern05@gmail.com>
2017-07-19 18:42:06 -0400

    added menu

---
+++ app/products_app.py
@@ -0,0 +1,6 @@
+
+menu = "Hello, welcome to the products app. There are 100 products. Please choose an operation: 'Create', 'list', 'show', 'update', 'destroy'"
+
+chosen_operation = input(menu)
+
+print(chosen_operation)
